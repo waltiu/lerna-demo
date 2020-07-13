39b6489e66fcc082e959b063be852a0f72500b8e
twomeetings <2326220@163.com>
2020-07-13 08:12:43 +0800

    update dangerfile

--- dangerfile.js
+++ dangerfile.js
@@ -1,13 +1,7 @@
-import { danger, fail} from 'danger'
+import { danger, fail, warn} from 'danger'
 
 fail('This is a failure message')
 
+warn('This is a warn message')
 
-// const currentMrAuthor = danger.gitlab.mr.author.username
-// const { modified_files, created_files, deleted_files } = danger.git
-// const allChangedFiles = [...modified_files, ...created_files, ...deleted_files]
-
-// const webAppFile = allChangedFiles.find(x=>x.indexOf('/web-app/')>-1)
-// if(webAppFile!==undefined && currentMrAuthor==='twomeetings1'){
-//   fail(`你没有权限提交代码到'web-app'项目，请先撤销相应文件修改，再尝试Merge Request的合并`)
-// }
+JSON.stringify(danger.github)
