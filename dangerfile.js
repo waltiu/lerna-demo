2d043bf326cfc82d78fb065ba41f506cb6423394
twomeetings <2326220@163.com>
2020-07-13 08:39:10 +0800

    print pr title

--- dangerfile.js
+++ dangerfile.js
@@ -4,5 +4,7 @@ fail('This is a failure message')
 
 warn('This is a warn message')
 
+console.warn('pr title:', danger.github.pr.title)
+
 console.warn('=== danger.github:')
 console.warn(JSON.stringify(danger.github))
