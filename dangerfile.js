1269ad8bf65c30419c30e49d82af0e8d9b523e39
twomeetings <2326220@163.com>
2020-07-14 06:19:04 +0800

    update dangerfile that title need to startwith feature

--- dangerfile.js
+++ dangerfile.js
@@ -4,7 +4,9 @@ fail('This is a failure message')
 
 warn('This is a warn message')
 
-if (danger.github.pr.title.startsWith('feature/')) {
+const { title } = danger.github.pr
+
+if (!title.startsWith('feature/')) {
 	fail('标题必须要由"feature/"开头')
 }
 
