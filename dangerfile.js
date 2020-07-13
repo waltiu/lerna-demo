b2424c7b11bc9faedad46d38f17157660d42ac66
twomeetings <2326220@163.com>
2020-07-13 08:09:00 +0800

    test github pr locally

--- dangerfile.js
+++ dangerfile.js
@@ -1,10 +1,13 @@
 import { danger, fail} from 'danger'
 
-const currentMrAuthor = danger.gitlab.mr.author.username
-const { modified_files, created_files, deleted_files } = danger.git
-const allChangedFiles = [...modified_files, ...created_files, ...deleted_files]
+fail('This is a failure message')
 
-const webAppFile = allChangedFiles.find(x=>x.indexOf('/web-app/')>-1)
-if(webAppFile!==undefined && currentMrAuthor==='twomeetings1'){
-  fail(`你没有权限提交代码到'web-app'项目，请先撤销相应文件修改，再尝试Merge Request的合并`)
-}
+
+// const currentMrAuthor = danger.gitlab.mr.author.username
+// const { modified_files, created_files, deleted_files } = danger.git
+// const allChangedFiles = [...modified_files, ...created_files, ...deleted_files]
+
+// const webAppFile = allChangedFiles.find(x=>x.indexOf('/web-app/')>-1)
+// if(webAppFile!==undefined && currentMrAuthor==='twomeetings1'){
+//   fail(`你没有权限提交代码到'web-app'项目，请先撤销相应文件修改，再尝试Merge Request的合并`)
+// }
